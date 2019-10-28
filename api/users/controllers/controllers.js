0e1364ba5cd4161e9099b4204b9c20529a5707f6
Florent Poinsard <florent.poinsard@outlook.fr>
2019-10-28 22:06:49 +0100

    [ADD] #1 - Architecture of the users api and addition to docker compose and GAE

---
+++ api/users/controllers/controllers.js
@@ -0,0 +1,11 @@
+// var model = require("../models/models")
+
+module.exports = {
+    home
+}
+
+function home(req, res) {
+    res.set('Content-Type', 'application/json');
+    res.status(200).json({ status: 'success', code: 200 })
+    return
+}
