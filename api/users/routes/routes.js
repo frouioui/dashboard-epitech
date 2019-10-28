1dd8dc553e08691ef6028aab2dd2373712286e9f
Florent Poinsard <florent.poinsard@outlook.fr>
2019-10-28 22:36:23 +0100

    [MOD] - Modification of the users api home roots for dispatch tests

--- api/users/routes/routes.js
+++ api/users/routes/routes.js
@@ -4,6 +4,9 @@ module.exports = function (app) {
     var controllers = require('../controllers/controllers');
 
     // Incomes
+    app.route('/home')
+        .get(controllers.home)
+
     app.route('/')
         .get(controllers.home)
 };
