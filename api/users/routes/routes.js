c88a8c501893158e85973c736903f3c7e4bb52be
Florent Poinsard <florent.poinsard@outlook.fr>
2019-10-28 23:01:35 +0100

    [MOD] - URL users app test with GAE

--- api/users/routes/routes.js
+++ api/users/routes/routes.js
@@ -4,9 +4,9 @@ module.exports = function (app) {
     var controllers = require('../controllers/controllers');
 
     // Incomes
-    app.route('/home')
+    app.route('/v1/users/home')
         .get(controllers.home)
 
-    app.route('/')
+    app.route('/v1/users')
         .get(controllers.home)
 };
