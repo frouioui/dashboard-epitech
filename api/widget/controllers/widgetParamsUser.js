229157613dc03a4ba3b679ae1c805303ef0ea3c2
Florent Poinsard <florent.poinsard@outlook.fr>
2019-10-31 16:22:30 +0100

    [ADD] #8 - Addition of the user widget and params and widget params

---
+++ api/widget/controllers/widgetParamsUser.js
@@ -0,0 +1,89 @@
+var model = require("../models/widgetParamsUser")
+
+module.exports = {
+    getAllUserWidgetParams,
+    getUserWidgetParamsByValue,
+    addOneUserWidgetParam,
+    deleteOneUserWidgetParam
+}
+
+function getAllUserWidgetParams(req, res) {
+    res.set('Content-Type', 'application/json');
+    model.getAllUserWidgetParams(req, res, (req, res, error, result) => {
+        if (error) {
+            console.error(error)
+            res.status(500).json({ status: 'failure', code: 500, data: { message: "API server error" } })
+        } else {
+            res.status(200).json({ status: 'success', code: 200, data: result })
+        }
+    })
+    return
+}
+
+function getUserWidgetParamsByValue(req, res) {
+    res.set('Content-Type', 'application/json');
+    let key = req.query.key
+    let value = req.query.value
+    if (key == "id") {
+        model.getOneUserWidgetParamsByID(req, res, value, (req, res, error, result) => {
+            if (error) {
+                console.error(error)
+                res.status(500).json({ status: 'failure', code: 500, data: { message: "API server error" } })
+            } else {
+                res.status(200).json({ status: 'success', code: 200, data: result })
+            }
+        })
+        return
+    } else if (key == "user_id") {
+        model.getUserWidgetParamsByUserID(req, res, value, (req, res, error, result) => {
+            if (error) {
+                console.error(error)
+                res.status(500).json({ status: 'failure', code: 500, data: { message: "API server error" } })
+            } else {
+                res.status(200).json({ status: 'success', code: 200, data: result })
+            }
+        })
+        return
+    } else if (key == "widget_user_id") {
+        model.getUserWidgetParamsByUserWidgetID(req, res, value, (req, res, error, result) => {
+            if (error) {
+                console.error(error)
+                res.status(500).json({ status: 'failure', code: 500, data: { message: "API server error" } })
+            } else {
+                res.status(200).json({ status: 'success', code: 200, data: result })
+            }
+        })
+        return
+    }
+    res.status(400).json({ status: 'failure', code: 400, data: { message: "Invalid 'key' = '" + key + "' param" } })
+    return
+}
+
+function addOneUserWidgetParam(req, res) {
+    res.set('Content-Type', 'application/json');
+    let data = { value: req.body.value, user_id: req.body.user_id, widget_user_id: req.body.widget_user_id, widget_param_id: req.body.widget_param_id }
+    model.addOneUserWidgetParam(req, res, data, (req, res, error, result) => {
+        if (error) {
+            console.error(error)
+            res.status(500).json({ status: 'failure', code: 500, data: { message: "API server error" } })
+        } else {
+            res.status(200).json({ status: 'success', code: 200, data: result.insertId })
+        }
+    })
+    return
+}
+
+function deleteOneUserWidgetParam(req, res) {
+    res.set('Content-Type', 'application/json');
+    let id = req.params.id
+    model.deleteUserWidgetParam(req, res, id, (req, res, error, result) => {
+        if (error) {
+            console.error(error)
+            res.status(500).json({ status: 'failure', code: 500, data: { message: "API server error" } })
+        } else {
+            res.status(200).json({ status: 'success', code: 200, data: { message: "Deleted" } })
+        }
+    })
+    return
+}
+
---
+++ api/widget/models/widgetParamsUser.js
@@ -0,0 +1,46 @@
+const { con } = require("./config")
+
+module.exports = {
+    getAllUserWidgetParams,
+    getOneUserWidgetParamsByID,
+    getUserWidgetParamsByUserID,
+    getUserWidgetParamsByUserWidgetID,
+    addOneUserWidgetParam,
+    deleteUserWidgetParam
+}
+
+function getAllUserWidgetParams(req, res, dataHandler) {
+    con.query(`SELECT * FROM api_widget.widget_user_params`, function (error, results, fields) {
+        dataHandler(req, res, error, results)
+    })
+}
+
+function getOneUserWidgetParamsByID(req, res, id, dataHandler) {
+    con.query(`SELECT * FROM api_widget.widget_user_params WHERE id = ` + id, function (error, results, fields) {
+        dataHandler(req, res, error, results)
+    })
+}
+
+function getUserWidgetParamsByUserID(req, res, id, dataHandler) {
+    con.query(`SELECT * FROM api_widget.widget_user_params WHERE user_id = ` + id, function (error, results, fields) {
+        dataHandler(req, res, error, results)
+    })
+}
+
+function getUserWidgetParamsByUserWidgetID(req, res, widget_user_id, dataHandler) {
+    con.query(`SELECT * FROM api_widget.widget_user_params WHERE user_widget_id = ` + widget_user_id, function (error, results, fields) {
+        dataHandler(req, res, error, results)
+    })
+}
+
+function addOneUserWidgetParam(req, res, data, dataHandler) {
+    con.query(`INSERT INTO api_widget.widget_user_params (user_id, user_widget_id, widget_param_id, value) VALUES (` + data.user_id + `, ` + data.widget_user_id + `, ` + data.widget_param_id + `, "` + data.value + `")`, function (error, results, fields) {
+        dataHandler(req, res, error, results)
+    })
+}
+
+function deleteUserWidgetParam(req, res, id, dataHandler) {
+    con.query(`DELETE FROM api_widget.widget_user_params WHERE id = ` + id, function (error, results, fields) {
+        dataHandler(req, res, error, results)
+    })
+}
