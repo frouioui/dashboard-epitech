48f5433e55781d36c4285f11e8c25a25775de633
Julien Ferrier <julien.ferrier@commiteo.com>
2019-11-01 22:30:13 +0100

    [ADD] the issus #33 is fixed

---
+++ app/src/pages/Signup.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { Link, Redirect } from 'react-router-dom';
+import { Card, Form, Input, Button, Error } from '../components/AuthForm';
+import axios from  'axios';
+import { useAuth } from "../context/auth";
+
+function Signup() {
+  const [isRegisterIn, setRegisterIn]
+  = useState(false);
+  const [isErrorRegister, setIsErrorRegister] = useState(false);
+  const [isMismatchPwd, setIsMismatchPwd] = useState(false);
+  const [email, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordconfirm] = useState("");
+  const { setAuthTokens } = 
+  useAuth();
+
+
+function postRegister() {
+  let url = "https://api.pedafy.com/v1/users/new"
+  if (password !== passwordConfirm ) {
+    console.log("PASSWORDS MISMATCH")
+    setIsMismatchPwd(true);
+  }
+  else axios.post(url, {
+    email: email,
+    password: password
+  }).then(result => {
+    console.log(result)
+    if (result.status === 200) {
+      console.log("SUCCESS")
+      setRegisterIn(true);
+    } else {
+      console.log("ERROR")
+      setIsErrorRegister(true);
+    }
+  }).catch(e => {
+    console.log(e)
+    setIsErrorRegister(true)
+    });
+  }
+
+if (isRegisterIn) {
+  return <Redirect to="/" />
+}
+
+    return (
+      <Card>
+        <Form>
+          <Input type="Adresse mail"
+          value={email}
+          onChange={e => {
+            setUserName(e.target.value);
+          }}
+           placeholder="Mail address" 
+           />
+          <Input type="password"
+          value={password}
+          onChange={e => {
+            setPassword(e.target.value);
+          }} 
+          placeholder="Password" 
+          />
+          <Input type="password"
+          value={passwordConfirm}
+          onChange={e => {
+            setPasswordconfirm(e.target.value);
+          }}
+          placeholder="Confirm your password"
+          />
+          <Button onClick={postRegister}>Sign Up</Button>
+        </Form>
+        <Link to="/login">Already have an account?</Link>
+        { isMismatchPwd &&<Error>Oups! The passwords seem to be different.</Error> }
+      </Card>
+    );
+  }
+
+
+export default Signup;
