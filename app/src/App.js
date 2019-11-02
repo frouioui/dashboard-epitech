bfa6d8cf2bd818e56d22b85d081579cb0efd8784
Julien Ferrier <julien.ferrier@commiteo.com>
2019-11-02 08:21:26 +0100

    [MODIFY] all files

---
+++ app/src/App.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import PrivateRoute from './PrivateRoute';
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import Login from "./pages/Login";
+import Signup from './pages/Signup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import { AuthContext } from "./context/auth";
+
+function App(props) {
+  const [authTokens, setAuthTokens] = useState();
+
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
+
+  return (
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens}}>
+      <Router>
+        <div>
+          <ul>
+          <li>
+            <Link to="/">Home Page</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin Page</Link>
+          </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <PrivateRoute path="/admin" component={Admin} />
+        </div>
+      </Router>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
---
+++ app/src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Home(props) {
+  return <div>Home Page</div>;
+}
+
+export default Home;
+
