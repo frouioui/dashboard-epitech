e66c997fd07290186b42ff32956555b8d4d8757f
Julien Ferrier <julien.ferrier@commiteo.com>
2019-11-03 13:34:14 +0100

    [ADD] #70 issue the header of the widget_page

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { useAuth } from "../context/auth";
+
+import "../CSS/html_properties_widgets_header.css"
 
 function Home(props) {
-  return <a href="/login"><div>Home Page</div></a>;
+  const { setAuthTokens } = useAuth();
+
+  function logOut() {
+    setAuthTokens();
+  }
+
+  return (
+    <div>
+       <div className="header">
+      <button>Pedafy</button>
+      </div>
+  <div class="dropdown">
+    <button class="dropbtn">Setting</button>
+      <div class="dropdown-content">
+        <a href="https://pedafy.com/signup">Add Widget</a>
+        <a href="https://pedafy.com/signup">Delete Widget</a>
+        <a href="#">Use our API</a>
+      </div>
+  </div>
+  <div className="footer3">
+  </div>
+    </div>
+  );
 }
 
 export default Home;
