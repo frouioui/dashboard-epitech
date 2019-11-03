77a1340af74bfeb859b1edde2ecedd5f35638fe4
Florent Poinsard <florent.poinsard@outlook.fr>
2019-11-03 23:24:13 +0100

    [ADD] - Intra API version

--- api/intra/main.go
+++ api/intra/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/frouioui/dashboard-epitech/api/intra/routes"
+
 	"github.com/gorilla/mux"
 )
 
@@ -13,17 +15,31 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
-		log.Printf("Using default port %s", port)
+		log.Printf("Using default port %s.", port)
 	}
 	r := mux.NewRouter()
 
+	routes.SetMiddleware(r)
+	routes.Assign(r)
+
 	s := &http.Server{
-		Addr:           port,
+		Addr:           ":" + port,
 		Handler:        r,
 		IdleTimeout:    5 * time.Second,
 		ReadTimeout:    5 * time.Second,
 		WriteTimeout:   5 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	log.Fatal(s.ListenAndServe())
+	waiter := make(chan error)
+	log.Println("Starting server ...")
+	go func() {
+		err := s.ListenAndServe()
+		waiter <- err
+	}()
+	log.Println("Server up and running.")
+	err := <-waiter
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Server shutted down.")
 }
--- api/intra/routes/middleware.go
+++ api/intra/routes/middleware.go
@@ -3,24 +3,28 @@ package routes
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func SetMiddleware() {
-
+// SetMiddleware set the middlewares used by mux router
+func SetMiddleware(r *mux.Router) {
+	log.Println("Setting up middlewares ...")
+	r.Use(setCORS)
+	r.Use(setJSONResponse)
+	log.Println("Middlewares set.")
 }
 
 func setCORS(next http.Handler) http.Handler {
-	log.Println("Setting CORS")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		next.ServeHTTP(w, r)
 	})
 }
 
 func setJSONResponse(next http.Handler) http.Handler {
-	log.Println("Setting JSON response")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
