5ddf2e454e8a982f68056a6dfeba31e09da2f53f
Florent Poinsard <florent.poinsard@outlook.fr>
2019-11-08 18:19:05 +0100

    [ADD] - Addition of console.log for debug in github api

--- api/users/controllers/controllers.js
+++ api/users/controllers/controllers.js
@@ -23,11 +23,13 @@ function githubLogin(req, res) {
             let login = jsonUser.login
             model.getUserByLogin(req, res, login, (req1, res1, err, resultDb) => {
                 if (err) {
+                    console.log(err)
                     res.status(500).json({ status: 'failure', code: 500, data: { message: "API server error" } })
                     return
                 } else if (resultDb.length == 0) {
                     model.addNewUserNoPassword(req, res, login, (req2, res2, err2, resultDb2) => {
                         if (err2) {
+                            console.log(err2)
                             res.status(500).json({ status: 'failure', code: 500, error: err })
                         } else {
                             res.status(200).json({ status: 'success', code: 200, data: { message: "Authenticated", id: resultDb2.insertId, auth: json.data.access_token } })
@@ -38,9 +40,11 @@ function githubLogin(req, res) {
                 }
             })
         }).catch((err) => setImmediate(() => {
+            console.log(err)
             res.status(500).json({ status: 'failure', code: 500, error: err })
         }))
     }).catch((err) => setImmediate(() => {
+        console.log(err)
         res.status(400).json({ status: 'failure', code: 400, error: err })
     }))
 }
