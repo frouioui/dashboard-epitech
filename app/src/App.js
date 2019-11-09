354a6357172932243eee6e23cb5cfe17a8692b0c
Florent Poinsard <florent.poinsard@outlook.fr>
2019-11-09 19:05:49 +0100

    [ADD] - Addition of the display of boxes for all widgets

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 import Signup from './pages/Signup';
-import Allwidget from './pages/Allwidget';
+import Allwidgets from './pages/Allwidget';
 import Addwidget from './pages/Addwidget';
 import CallbackOauth from './pages/CallbackOauth';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -29,7 +29,7 @@ function App(props) {
           <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
-          <Route path="/allwidget" component={Allwidget} />
+          <Route path="/allwidget" component={Allwidgets} />
           <Route path="/addwidget" component={Addwidget} />
           <Route path="/admin" component={Admin} />
           <Route path="/auth/github" component={CallbackOauth} />
--- app/src/pages/Allwidget.js
+++ app/src/pages/Allwidget.js
@@ -1,50 +1,108 @@
 import React from "react";
-import { useAuth } from "../context/auth";
 import Cookies from 'universal-cookie';
 import { Redirect } from 'react-router-dom'
+import { getAllWidgetsOfOneUser } from "../client/widgets";
 
-import "../CSS/html_properties_widgets_header.css"
+import "../CSS/allwidgets.css"
 
-function Home(props) {
-  const { setAuthTokens } = useAuth();
+class Allwidgets extends React.Component {
+  constructor(props) {
+    super(props);
 
-  function logOut() {
-    setAuthTokens();
+    var cookies = new Cookies()
+
+    this.state = {
+      userWidgets: [],
+      userWidgetLoaded: false,
+      user_id: cookies.get('user_id'),
+      auth: cookies.get('auth'),
+      isError: false
+    }
   }
 
-  const cookies = new Cookies();
-  let user_id = cookies.get('user_id')
-  if (!user_id || user_id === "") {
-    return <Redirect to='/login' />
+  componentDidMount() {
+    getAllWidgetsOfOneUser(this.state.user_id).then(res => {
+      this.setState({
+        userWidgets: res.data,
+        userWidgetLoaded: true
+      })
+    }).catch((err) => setImmediate(() => {
+      console.error(err)
+      this.setState({
+        isError: true
+      })
+    }))
   }
 
-  return (
-    <div>
-      <div className="header">
-        <button>Pedafy</button>
-      </div>
-      <div class="dropdown">
-        <button class="dropbtn">Setting</button>
-        <div class="dropdown-content">
-          <a href="https://pedafy.com/signup">Add Widget</a>
-          <a href="https://pedafy.com/signup">Logout</a>
-          <a href="#">Use our API</a>
+  header() {
+    return (
+      <div>
+        <div className="header">
+          <button>Pedafy</button>
+        </div>
+        <div class="dropdown">
+          <button class="dropbtn">Setting</button>
+          <div class="dropdown-content">
+            <a href="#">Add Widget</a>
+            <a href="#">Logout</a>
+            <a href="#">Use our API</a>
+          </div>
         </div>
       </div>
-      <div className="body">
-        <form method="get" action="/addwidget">
-          <button type="submit">Add a widget</button>
-        </form>
+    )
+  }
+
+  displayWidgets() {
+    return (
+      <div>
+        {this.state.userWidgets.map(widget => (
+          <div class="column">
+            <div class="widgetsbox">
+              <h6>{widget.id}</h6>
+            </div>
+          </div>
+        ))}
+        {this.state.userWidgets.map(widget => (
+          <div class="column">
+            <div class="widgetsbox">
+              <h6>{widget.id}</h6>
+            </div>
+          </div>
+        ))
+        }
       </div>
-      <div className="test">
-        <form method="get" action="/test">
-          <button type="submit">See all services</button>
-        </form>
+    )
+  }
+
+  render() {
+    if (!this.state.user_id || this.state.user_id === "") {
+      return <Redirect to='/login' />
+    }
+
+    if (this.state.isError == true) {
+      return <p>An error occured, contact the maintainer of the website.</p>
+    } else if (this.state.userWidgetLoaded == false) {
+      return <p>Loading ...</p>
+    }
+
+    return (
+      <div>
+        {this.header()}
+        <div className="body container">
+          <form method="get" action="/addwidget">
+            <button type="submit">Add a widget</button>
+          </form>
+        </div>
+        <div class="container-widget">
+          {this.displayWidgets()}
+          {this.displayWidgets()}
+          {this.displayWidgets()}
+          {this.displayWidgets()}
+        </div>
       </div>
-      <div className="footer4">
-      </div>
-    </div>
-  );
+
+    );
+  }
 }
 
-export default Home;
+export default Allwidgets;
